d085177b653935515d61d5479689eea36c6347b2
Dileep245 <dileepks615@gmail.com>
2026-06-04 15:59:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Agriculture Dashboard",
+    page_icon="🌾",
+    layout="wide"
+)
+
+with open("assets/custom.css") as f:
+    st.markdown(
+        f"<style>{f.read()}</style>",
+        unsafe_allow_html=True
+    )
+
+st.title("🌾 Indian Agriculture Analytics Dashboard")
+
+st.markdown("""
+Welcome to Agriculture Analytics Dashboard.
+
+Use the navigation panel on the left.
+""")
